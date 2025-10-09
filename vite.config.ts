8fead1a04dad988b52487fa38470acc450240e38
Deepfresh-star <deep.vithani@freshcodes.net>
2025-10-09 13:10:33 +0530

    feat : chunkerror solve

--- vite.config.ts
+++ vite.config.ts
@@ -21,6 +21,24 @@ export default defineConfig({
   },
   build: {
     outDir: "build",
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Vendor chunks
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+          'redux-vendor': ['@reduxjs/toolkit', 'react-redux', 'redux-persist'],
+          'ui-vendor': ['framer-motion', 'react-icons', 'react-select', 'react-modal'],
+          'chart-vendor': ['apexcharts', 'react-apexcharts', '@visx/pattern'],
+          'form-vendor': ['formik', 'yup', 'react-number-format'],
+          'table-vendor': ['@tanstack/react-table', '@tanstack/match-sorter-utils'],
+          'calendar-vendor': ['@fullcalendar/react', '@fullcalendar/daygrid', '@fullcalendar/timegrid', '@fullcalendar/interaction'],
+          'utils-vendor': ['lodash', 'dayjs', 'axios', 'classnames'],
+          'editor-vendor': ['react-quill', 'react-syntax-highlighter', 'html-react-parser'],
+          'misc-vendor': ['swiper', 'react-beautiful-dnd', 'react-scroll', 'react-tooltip']
+        }
+      }
+    }
   },
   // preview: {
   //   allowedHosts: ["pricefaster.onrender.com"], // Allow your Render domain
